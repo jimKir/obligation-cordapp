6ea51903bee409a11d0455084f21fe9547224454
kiritsis.di@gmail.com <kiritsis.di@gmail.com>
2018-01-16 16:29:15 +0100

    Support for work state

---
+++ java-source/src/main/java/net/corda/examples/obligation/flows/IssueWork.java
@@ -0,0 +1,111 @@
+package net.corda.examples.obligation.flows;
+
+import co.paralleluniverse.fibers.Suspendable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import net.corda.core.contracts.Amount;
+import net.corda.core.flows.*;
+import net.corda.core.identity.Party;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+import net.corda.core.utilities.ProgressTracker;
+import net.corda.examples.obligation.Work;
+import net.corda.examples.obligation.WorkContract;
+
+import java.security.PublicKey;
+import java.time.Duration;
+import java.util.Currency;
+import java.util.List;
+
+/**
+ * Created by pai on 16.01.18.
+ */
+public class IssueWork {
+
+    @InitiatingFlow
+    @StartableByRPC
+    public static class Initiator extends ObligationBaseFlow {
+
+        private final Amount<Currency> amount;
+        private final Party lender;
+        private String featureTitle;
+        private String description;
+        //private final Boolean anonymous;
+
+        private final ProgressTracker.Step INITIALISING = new ProgressTracker.Step("Performing initial steps.");
+        private final ProgressTracker.Step BUILDING = new ProgressTracker.Step("Performing initial steps.");
+        private final ProgressTracker.Step SIGNING = new ProgressTracker.Step("Signing transaction.");
+        private final ProgressTracker.Step COLLECTING = new ProgressTracker.Step("Collecting counterparty signature.") {
+            @Override public ProgressTracker childProgressTracker() {
+                return CollectSignaturesFlow.Companion.tracker();
+            }
+        };
+        private final ProgressTracker.Step FINALISING = new ProgressTracker.Step("Finalising transaction.") {
+            @Override public ProgressTracker childProgressTracker() {
+                return FinalityFlow.Companion.tracker();
+            }
+        };
+
+        private final ProgressTracker progressTracker = new ProgressTracker(
+                INITIALISING, BUILDING, SIGNING, COLLECTING, FINALISING
+        );
+
+        public Initiator(Amount<Currency> amount, Party lender, String featureTitle, String description) {
+            this.amount = amount;
+            this.lender = lender;
+            this.featureTitle = featureTitle;
+            this.description = description;
+
+        }
+
+        @Override
+        public ProgressTracker getProgressTracker() {
+            return progressTracker;
+        }
+
+
+        @Suspendable
+        private Work createWork() throws FlowException {
+            return new Work(featureTitle, description, amount, lender, getOurIdentity());
+        }
+
+        @Suspendable
+        @Override
+        public SignedTransaction call() throws FlowException {
+            // Step 1. Initialisation.
+            progressTracker.setCurrentStep(INITIALISING);
+            final Work work = createWork();
+            final PublicKey ourSigningKey = work.getBorrower().getOwningKey();
+
+            // Step 2. Building.
+            progressTracker.setCurrentStep(BUILDING);
+            final List<PublicKey> requiredSigners = work.getParticipantKeys();
+
+            final TransactionBuilder utx = new TransactionBuilder(getFirstNotary())
+                    .addOutputState(work, WorkContract.WORK_CONTRACT_ID)
+                    .addCommand(new WorkContract.Commands.Issue(), requiredSigners)
+                    .setTimeWindow(getServiceHub().getClock().instant(), Duration.ofSeconds(30));
+
+            // Step 3. Sign the transaction.
+            progressTracker.setCurrentStep(SIGNING);
+            final SignedTransaction ptx = getServiceHub().signInitialTransaction(utx, ourSigningKey);
+
+            // Step 4. Get the counter-party signature.
+            progressTracker.setCurrentStep(COLLECTING);
+            final FlowSession lenderFlow = initiateFlow(lender);
+            final SignedTransaction stx = subFlow(new CollectSignaturesFlow(
+                    ptx,
+                    ImmutableSet.of(lenderFlow),
+                    ImmutableList.of(ourSigningKey),
+                    COLLECTING.childProgressTracker())
+            );
+
+            // Step 5. Finalise the transaction.
+            progressTracker.setCurrentStep(FINALISING);
+            return subFlow(new FinalityFlow(stx, FINALISING.childProgressTracker()));
+
+        }
+    }
+
+
+}
